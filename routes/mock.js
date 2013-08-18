4d3c92eae4f01505fa8b7ebdaaa3c0b9ca6daef9
Pablo Botelho <pablo.bcb@gmail.com>
2013-08-18 00:09:29 -0300

    mock for get all profiles done

---
+++ routes/mock.js
@@ -0,0 +1,64 @@
+exports.profile = {}
+exports.profile.all = [{
+	  "id" 				 : 10,
+	  "name" 			 : "Alessandro Delgado",
+	  "email" 			 : "adelgado@website.com",
+	  "phone1" 			 : "12345678", 
+	  "phone2" 			 : "23456789", 
+	  "qualifications" : "Bacharel Degree on CS(on course) at Puc-Rio",
+	  "birthDate"		 : "01/02/1990",
+	  "topics"         : ["javascript", "nodejs", "expressjs", "coffee-script",
+	  							"bdd", "tdd", "lua", "functional programming", "css",
+								"html5", "linux", "opensource", "git", "mongodb", "sql"]
+  },
+  {
+	  "id" 				 : 11,
+	  "name" 			 : "Pablo Botelho",
+	  "email" 			 : "pbotelho@website.com",
+	  "phone1" 			 : "92345678", 
+	  "phone2" 			 : "93456789", 
+	  "qualifications" : "Bacharel Degree on System Analysis(on course) at Puc-Rio",
+	  "birthDate"		 : "01/02/1990",
+	  "topics"         : ["javascript", "nodejs", "expressjs", "python", "Java",
+	  							"bdd", "tdd", "lua", "functional programming", "android",
+								"C", "linux", "opensource", "git", "mongodb", "sql"]
+  },
+  {
+	  "id" 				 : 18,
+	  "name" 			 : "Anders Heijsberg",
+	  "email" 			 : "aheijsberg@website.com",
+	  "phone1" 			 : "92325478", 
+	  "phone2" 			 : "90450789", 
+	  "qualifications" : "Probably a lot of stuff at some fancy schools",
+	  "birthDate"		 : "01/02/1965",
+	  "topics"         : ["c#", "C", ".NET", "link", "Microsoft SQL Server",
+	  							"bdd", "tdd", "functional programming", "windows phone",
+								"C", "windows", "git", "c++", "Active Directory", "sql"]
+  },
+  {
+	  "id" 				 : 19,
+	  "name" 			 : "Douglas Crockford",
+	  "email" 			 : "dcrock@website.com",
+	  "phone1" 			 : "92345878", 
+	  "phone2" 			 : "93452689", 
+	  "qualifications" : "Old school stuff for sure",
+	  "birthDate"		 : "01/02/1890",
+	  "topics"         : ["javascript", "nodejs", "Java", "scheme", "smalltalk", 
+	  							"bdd", "tdd", "json", "functional programming", "jslinter",
+								"C", "linux", "need for semicolons", "git", "mongodb", "sql"]
+  }
+  ,{
+	  "id" 				 : 61,
+	  "name" 			 : "Larry Wall",
+	  "email" 			 : "lwall@website.com",
+	  "phone1" 			 : "82345678", 
+	  "phone2" 			 : "23450789", 
+	  "qualifications" : "Linguistics, NASA and a lot of crazy shit",
+	  "birthDate"		 : "01/02/1990",
+	  "topics"         : ["perl", "camels", "usenet", "code obfuscation", "perl6",
+								"C", "linux", "opensource" ]
+  }]
+exports.profile.get    = {}
+exports.profile.create = {}
+exports.profile.update = {}
+exports.profile.delete = {}
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,23 @@
+var mock = require('./mock').profile
 
-/*
- * GET users listing.
- */
+exports.all = function(req, res){
+  console.log(mock)
+  console.log('passei')
+  res.json(mock.all)
+}
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
-};
+exports.delete = function(req,res){
+
+}
+
+exports.update = function(req,res){
+
+}
+
+exports.create = function(req,res){
+
+}
+
+exports.get = function(req,res){
+
+}
