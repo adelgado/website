0b02b94856ca9ca6076a23fd39e3120e114697a9
Alessandro Delgado <alessandro@delgado.githubdoesnotsupporemailsthatdontlooklikeemails>
2013-08-20 00:20:35 +0200

    Estilo

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.mongo = { 
-	getUrl : function(){
-					var user   = 'website'
-					var pass   = 'website'
-					var schema = 'website'
-					var host   = 'localhost'
-					var port   = 27017
-					return 'mongodb://' + user + ':' + pass + '@' + host + ':' + port + '/' + schema
-				}
+	getUrl : function () {
+		var user   = 'website'
+		var pass   = 'website'
+		var schema = 'website'
+		var host   = 'localhost'
+		var port   = 27017
+		return 'mongodb://' + user + ':' + pass + '@' + host + ':' + port + '/' + schema
+	}
 }
